0661657f4cdac2fd330392b43215e842438b7090
Isabella Neel <ineel@terpmail.umd.edu>
2023-02-20 18:54:46 -0500

    added testing for pacman consume, need to come back to this later

--- Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
@@ -6,6 +6,21 @@ import java.awt.Color;
 public class TestPacManConsume extends TestCase {
 
   public void testPacManConsume() throws FileNotFoundException {
+    //Creating A Map
+    Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    
+
+    //Creating Players
+    PacMan pacman = frame.addPacMan(new Location(2, 2)); //Creates PacMan at location x, y
+    if(pacman.consume() == null){
+      
+    }
+    frame.add("cookie", new Location(2, 2), null, Map.Type.COOKIE);
+    if(pacman.consume() != null){
+      
+    }
+    
     return;
+
   }
 }
