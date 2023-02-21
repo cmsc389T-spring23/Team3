1b9d6b7ce5d265fe554119e648ff31ecb4b28325
mahinsa2345 <mahinsa.sd@gmail.com>
2023-02-20 20:00:06 -0500

    test for map getLoc

--- Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
+++ Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
@@ -2,10 +2,24 @@ package pacman;
 import java.io.*;
 import junit.framework.*;
 import java.awt.Color;
+import java.util.HashSet;
+import pacman.Map.Type;
 
 public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
+    NoFrame frame = new NoFrame();
+    Map map = frame.getMap();
+    HashSet<Type> test = new HashSet<Type>();
+    assertEquals(map.getLoc(new Location (-1,-1)), null);
+    test.add(Map.Type.WALL);
+    assertEquals(map.getLoc(new Location (2,4)), test);
+    test.remove(Map.Type.WALL);
+    test.add(Map.Type.COOKIE);
+    frame.addPacMan(new Location(1, 1));
+    map = frame.getMap();
+    test.add(Map.Type.PACMAN);
+    assertEquals(map.getLoc(new Location (1,1)), test);
     return;
   }
 }
