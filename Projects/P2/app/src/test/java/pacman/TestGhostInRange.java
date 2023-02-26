fc767ff0696af6c86b95761bb0a910b1132ff4db
adidriks <123581191+adidriks@users.noreply.github.com>
2023-02-25 21:24:41 -0500

    Update TestGhostInRange.java

--- Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
@@ -6,12 +6,13 @@ import java.awt.Color;
 public class TestGhostInRange extends TestCase {
 
   public void testGhostInRange() throws FileNotFoundException {
-    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
+   NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
     
     Ghost ghost = frame.addGhost(new Location(1,2),"name",Color.red);
     PacMan pacman1 = frame.addPacMan(new Location(1, 1));
     
-    assertTrue(pacman1.is_ghost_in_range() == true);
+    assertTrue(ghost.is_pacman_in_range() == true);
+
 
   }
 }
