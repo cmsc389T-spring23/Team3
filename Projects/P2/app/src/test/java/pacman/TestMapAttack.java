489b3175c93b850397ef998bdcbc8d6e97072b76
adidriks <123581191+adidriks@users.noreply.github.com>
2023-02-25 21:21:19 -0500

    added testMapAttack

--- Projects/P2/app/src/test/java/pacman/TestMapAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestMapAttack.java
@@ -6,6 +6,12 @@ import java.awt.Color;
 public class TestMapAttack extends TestCase {
 
   public void testMapAttack() throws FileNotFoundException {
-    return;
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
+    
+    Ghost ghost = frame.addGhost(new Location(1,2),"name",Color.red);
+    PacMan pacman1 = frame.addPacMan(new Location(1, 1));
+    
+    assertTrue(frame.getMap().attack("name") == true);
+
   }
 }
