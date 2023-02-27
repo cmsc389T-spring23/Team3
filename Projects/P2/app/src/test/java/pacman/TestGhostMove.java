d9061b58af0bb834c28e3f09189397ff846316ce
mahinsa2345 <mahinsa.sd@gmail.com>
2023-02-27 11:32:23 -0500

    ghost move function and tests

--- Projects/P2/app/src/test/java/pacman/TestGhostMove.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostMove.java
@@ -6,6 +6,11 @@ import java.awt.Color;
 public class TestGhostMove extends TestCase {
 
   public void testGhostMove() throws FileNotFoundException {
-
+    NoFrame frame = new NoFrame();
+    Map map = frame.getMap();
+    Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red);
+    ArrayList<Location> moves = ghost.get_valid_moves();
+    assertTrue(ghost.move());
+    assertTrue(moves.contains(ghost.myLoc));
   }
 }
