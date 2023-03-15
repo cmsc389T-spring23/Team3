8fa1646a988a49632fa3c4725d948a43e1da7f84
IsabellaNeel <91357970+IsabellaNeel@users.noreply.github.com>
2023-03-15 12:51:58 -0400

    Update TestGhostAttack.java

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
-	  MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+	  NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
 
 	    PacMan pacman1 = frame.addPacMan(new Location(12, 12));
 	    Ghost ghost1 = frame.addGhost(new Location(1, 1), "Inky" , Color.red);
