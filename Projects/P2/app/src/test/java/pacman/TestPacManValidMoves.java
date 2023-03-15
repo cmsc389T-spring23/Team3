5b89c6bed878fb560c661d583ba6f2dfa4ddf6cc
Jasneet Kaur <jkaur106@umd.edu>
2023-03-15 16:27:23 -0400

    adding jasneet tests

--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -9,7 +9,7 @@ public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
     //Creating A Map
-    MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
 
     PacMan pacman1 = frame.addPacMan(new Location(1, 1)); //Pacman at location 2,2
     ArrayList<Location> moves1 = pacman1.get_valid_moves();
